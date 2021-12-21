ff78a0b6800c0b8c0dafff8d7e9290200347f133
sevass <sevarakodirova7711@gmail.com>
2021-12-21 22:49:03 +0500

    js homework

---
+++ script.js
@@ -0,0 +1,16 @@
+// odam 3 ta son kiritadi. uning o'rta arifmetigini topish kerak
+let num = +prompt('Write ok to calculate average of any three numbers');
+let num1 = +prompt('first')
+console.log(num1);
+
+let num2 = +prompt('second')
+console.log(num2);
+
+let num3 = +prompt('third')
+console.log(num3);
+
+alert('you got ' +(num1 + num2 + num3 ) /3)
+
+
+
+// console.log((num1 + num2 + num3 ) /3 );(yoki bu yo'l)
